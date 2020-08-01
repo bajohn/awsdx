6695634b7d4a5f48f7323eae3ecc769c0c67fa7c
Brendan John <bjohn454@gmail.com>
2020-08-01 16:38:30 -0400

    one-off file moving dx->s3

--- scripts/data_agg.py
+++ scripts/data_agg.py
@@ -1,18 +1,96 @@
 import boto3
+import gzip
+import time
+from smart_open import open as open
 
 
 def main():
 
-    client = boto3.client('dataexchange')
-    nasdaq_data_id = '7f9633003e50399a699344131fdf3b73'
+    dxClient = boto3.client('dataexchange')
 
-    revisions = list_data_source_revisions(client, nasdaq_data_id)
-    print(len(revisions))
+    nasdaq_data_id = '7f9633003e50399a699344131fdf3b73'
+    bucket = 'awsdx-data-bucket'
+    key = 'securities_data/Equities_XNAS_20200311.csv'
+
+    revisions = data_source_revisions(dxClient, nasdaq_data_id)
+
+    revision1 = revisions[0]
+
+    s3Key = dxS3Key(revision1, 'awsdx-data-bucket',
+                    'securities_data/', dxClient)
+    print(f'Done {s3Key}')
 
     return 'Done'
 
+    #     f = gzip.open('Equities_XNAS_20200311.csv', 'rb')
+    #     file_content = f.read()
+    # print(file_content)
+    # with open('outfile.csv', 'wb+') as outfile:
+    #     outfile.write(file_content)
+    # f.close()
 
-def list_data_source_meta(client):
+
+# data exchange object -> s3
+def dxS3Key(dxObj, s3Bucket, s3Prefix, dxClient):
+    name = dxObj['Comment']
+    revisionId = dxObj['Id']
+    dataSetId = dxObj['DataSetId']
+
+    revisionAssets = dxClient.list_revision_assets(
+        DataSetId=dataSetId,
+        RevisionId=revisionId
+    )
+
+    assetid = revisionAssets['Assets'][0]['Id']
+    s3Key = s3Prefix + name
+    createRequest = dict(
+        ExportAssetsToS3=dict(
+            DataSetId=dataSetId,
+            RevisionId=revisionId,
+            AssetDestinations=[
+                dict(
+                    AssetId=assetid,
+                    Bucket=s3Bucket,
+                    Key=s3Key,
+                )
+            ]
+        )
+    )
+    # print(createRequest)
+
+    resp = dxClient.create_job(
+        Details=createRequest, Type='EXPORT_ASSETS_TO_S3')
+
+    jobId = resp['Id']
+    dxClient.start_job(
+        JobId=jobId
+    )
+
+    while not jobComplete(jobId, dxClient):
+        time.sleep(1)
+
+    unzip_file(s3Bucket, s3Key)
+
+    return s3Key
+
+
+def jobComplete(jobId, dxClient):
+    jobResp = dxClient.get_job(JobId=jobId)
+    state = jobResp['State']
+    print(f'Job status {state}')
+    return state == 'COMPLETED'
+
+
+def unzip_file(bucket, key):
+    s3Client = boto3.client('s3')
+    fileLocation = f's3://{bucket}/{key}'
+    with open(fileLocation, 'rb') as zippedFile:
+        decompressedFile = gzip.decompress(zippedFile.read())
+        with open(fileLocation, 'wb') as outfile:
+            return outfile.write(decompressedFile)
+
+
+def data_source_meta(client):
     resp = client.list_data_sets(Origin='ENTITLED')
     dataSets = resp['DataSets']
     parsedDataSets = [dict(
@@ -23,14 +101,14 @@ def list_data_source_meta(client):
     return parsedDataSets
 
 
-def list_data_source_revisions(client, dataSetId, nextToken='', lastRevs=[]):
+def data_source_revisions(client, dataSetId, nextToken='', lastRevs=[]):
     botoArgs = dict(DataSetId=dataSetId)
     if nextToken:
         botoArgs['NextToken'] = nextToken
     resp = client.list_data_set_revisions(**botoArgs)
     revs = resp['Revisions'] + lastRevs
     if 'NextToken' in resp:
-        return list_data_source_revisions(
+        return data_source_revisions(
             client, dataSetId, nextToken=resp['NextToken'], lastRevs=revs)
     else:
         return revs
