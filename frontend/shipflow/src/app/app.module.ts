a5446fad82da0e112708dfbcc2e3983eafe59bac
Brendan John <bjohn454@gmail.com>
2020-08-29 14:15:14 -0400

    map rendering

--- frontend/shipflow/src/app/app.module.ts
+++ frontend/shipflow/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxMapboxGLModule.withConfig({
+      accessToken: 'pk.eyJ1IjoiYnJlbmRhbmFqb2huIiwiYSI6ImNrZWM1aHkwZjA0YzIydHJzaTdudHA5cjQifQ.ogTyplFS7LdAC7QspnVVkQ',
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
