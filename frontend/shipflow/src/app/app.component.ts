8c7c8b186e68ae91ed4e02c95c9ea18fb6b8e1b1
Brendan John <bjohn454@gmail.com>
2020-08-30 16:59:28 -0400

    cleanup; remove greatcircle / deck

--- frontend/shipflow/src/app/app.component.ts
+++ frontend/shipflow/src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
 import { v4 as uuidv4 } from 'uuid';
 
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import { Map } from 'mapbox-gl';
+
 
 @Component({
   selector: 'app-root',
@@ -14,11 +17,13 @@ export class AppComponent implements OnInit {
   defaultCenter = [-77.0366, 38.895]; //[-98.585522, 39.8333333];
   //londonishCenter = [0, 51.5];
 
-  appTime = new Date('2020-08-25');
+  appTime = new Date('2020-08-23');
   timeRate = 2;// clock seconds per app day
   updateRate = 100;// rerenders per second
   shipSpeed = 37; // 37 kph is approx 20 knots 
   daysToShow = 7; // number of days to show a ship for
+
+  map: Map;
 
   shipArr: {
     startLoc: string
@@ -26,9 +31,11 @@ export class AppComponent implements OnInit {
     startCoord: number[]
     endCoord: number[]
     endDate: Date
-    curSource: any
+    sourceData: any
+    srcId: string
+    ptId: string
+    lineId: string
   }[] = [];
-
 
 
   constructor() {
@@ -40,25 +47,36 @@ export class AppComponent implements OnInit {
     // start app clock
     setInterval(() => { this.updateAppTime() }, 1000 / this.updateRate)
 
-    const startLoc = 'New York';
-    const endLoc = 'Berlin';
+    const startLoc = 'brasilia';
+    const endLoc = 'new york';
     const endDate = new Date();
     const testObj = await this.initShipObj(startLoc, endLoc, endDate);
     this.shipArr.push(testObj);
   }
 
+
   async initShipObj(startLoc, endLoc, endDate) {
     const startCoord = await this.coord(startLoc);
     const endCoord = await this.coord(endLoc);
+    const data = {
+      from: {
+        coordinates: startCoord
+      },
+      to: {
+        coordinates: endCoord
+      }
+    }
+
     return {
       startLoc: startLoc,
       endLoc: endLoc,
       startCoord: startCoord,
       endCoord: endCoord,
       endDate: endDate,
-      curSource: this.pointSource(startCoord, endCoord, endDate),
+      sourceData: this.pointSource(startCoord, endCoord, endDate),
       srcId: uuidv4(),
-      ptId: uuidv4()
+      ptId: uuidv4(),
+      lineId: uuidv4()
     }
   }
 
@@ -67,20 +85,6 @@ export class AppComponent implements OnInit {
     const newTime = curTime + 24 * 3600 * 1000 / this.updateRate / this.timeRate;
     this.appTime = new Date(newTime);
     this.updateShips();
-  }
-
-  async doRequest() {
-    // this.testPoint.coordinates = respCoords;
-
-    // this.timer = window.setInterval(() => {
-    //   //console.log(this.testPoint.coordinates);
-    //   this.testPoint.coordinates = [
-    //     this.testPoint.coordinates[0] + .05,
-    //     this.testPoint.coordinates[1] - .05]
-
-    //   this.testPoint = { ...this.testPoint };
-    // }, 1);
-
   }
 
 
@@ -100,7 +104,6 @@ export class AppComponent implements OnInit {
     const bestfeature = features[0];
     return bestfeature['center'];
   }
-
 
 
 
@@ -133,7 +136,8 @@ export class AppComponent implements OnInit {
     const daysLeft = (endDate.getTime() - this.appTime.getTime()) / (24 * 3600 * 1000);
     const pct = daysLeft / this.daysToShow;
 
-    return [endCoord[0] + deltaX * pct, endCoord[1] + deltaY * pct];
+    //return [(1-endCoord[0] + deltaX * pct, endCoord[1] + deltaY * pct];
+    return [((1 - pct) * startCoord[0]) + endCoord[0] * pct, (1 - pct) * startCoord[1] + endCoord[1] * pct]
   }
 
   pointSource(startCoord, endCoord, endDate) {
@@ -150,9 +154,15 @@ export class AppComponent implements OnInit {
     for (let i in this.shipArr) {
       const curObj = this.shipArr[i]
       const ret = { ...curObj };
-      this.shipArr[i].curSource = this.pointSource(ret.startCoord, ret.endCoord, ret.endDate);
+      this.shipArr[i].sourceData = this.pointSource(ret.startCoord, ret.endCoord, ret.endDate);
     }
   }
+
+  // Not yet used but could be useful.
+  saveMap(map: Map) {
+    this.map = map;
+  }
+
 }
 
 
